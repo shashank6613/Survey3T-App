9c36163e0bd3cb1a45692074dbc6d75632c99d52
root <root@shashank-pc>
2024-09-07 11:13:36 +0530

    push

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,10 @@ const port = 8081;
 
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: 'newdb.clushmnnhufs.us-west-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'admin1234',
+  database: 'survey'
 });
 
 db.connect(err => {
